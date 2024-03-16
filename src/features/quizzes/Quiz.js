e8a403bfb9f709db5ad2462a37578d5cc6987a59
Emilio <emilio.pg95@gmail.com>
2024-03-15 20:34:24 -0600

    quizzes feature complete

--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { selectQuizzes } from "./quizzesSlice";
 import { Link, useParams, Navigate } from "react-router-dom";
 import Card from "../cards/Card";
 import ROUTES from "../../app/routes";
 // import quiz selector
 
 export default function Quiz() {
-  const quizzes = {}; // replace this with a call to your selector to get all the quizzes in state
+  const quizzes = useSelector(selectQuizzes)
   const { quizId } = useParams();
   const quiz = quizzes[quizId];
 
