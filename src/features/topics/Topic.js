8c3805f7c714ab9805f26f012764c5187fed76f0
Emilio <emilio.pg95@gmail.com>
2024-03-15 19:54:37 -0600

    Topic portion completed

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { selectTopics } from "./topicsSlice";
 import { Link, useParams, Navigate } from "react-router-dom";
 import ROUTES from "../../app/routes";
 // import selectors
 
 export default function Topic() {
-  const topics = {};  // replace with selector
+  const topics = useSelector(selectTopics);  // replace with selector
   const quizzes = {}; // replace with selector
   const { topicId } = useParams();
   const topic = topics[topicId];
